7fb0297563c4cefd42881fc1a109606194d485cc
Marcos Rebello <markkellmao@gmail.com>
2024-06-21 22:35:08 -0300

    first commit

---
+++ src/Teste.java
@@ -0,0 +1,7 @@
+public class Teste {
+          public static void main(String[] args) {
+                    String[] nomes = {"Camila", "Venilton", "Leonardo", "Renan", "Rafael"};
+                    System.out.print(nomes.length);    
+}
+
+}
